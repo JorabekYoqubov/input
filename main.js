3e48c2e889ec963d9ae6c24d4c476f51dd083b10
Yakubov Jo'rabek <107387452+JorabekYoqubov@users.noreply.github.com>
2023-09-22 18:21:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+let inp_1 = document.getElementById('inp_1')
+let inp_2 = document.getElementById('inp_2')
+let inp_3 = document.getElementById('inp_3')
+let inp_4 = document.getElementById('inp_4')
+let btn_1 = document.getElementById('btn_1')
+let btn_2 = document.getElementById('btn_2')
+let btn_3 = document.getElementById('btn_3')
+let btn_4 = document.getElementById('btn_4')
+let inp_5 = document.getElementById('inp_5')
+let inp_6 = document.getElementById('inp_6')
+let inp_7 = document.getElementById('inp_7')  
+let inp_8 = document.getElementById('inp_8')
+let btn_5 = document.getElementById('btn_5')
+let btn_6 = document.getElementById('btn_6')
+let btn_7 = document.getElementById('btn_7')
+let btn_8 = document.getElementById('btn_8')
+let inp_9 = document.getElementById('inp_9')
+let inp_10 = document.getElementById('inp_10')
+let inp_11 = document.getElementById('inp_11')
+let inp_12 = document.getElementById('inp_12')
+let btn_9 = document.getElementById('btn_9')
+let btn_10 = document.getElementById('btn_10')
+let btn_11 = document.getElementById('btn_11')
+let btn_12 = document.getElementById('btn_12')
+let inp_13 = document.getElementById('inp_13')
+let inp_14 = document.getElementById('inp_14')
+let inp_15 = document.getElementById('inp_15')
+let inp_16 = document.getElementById('inp_16')
+let btn_13 = document.getElementById('btn_13')
+let btn_14 = document.getElementById('btn_14')
+let btn_15 = document.getElementById('btn_15')
+let btn_16 = document.getElementById('btn_16')
+let h1_1 = document.getElementById('h1_1')
+let demo_1 = document.getElementById('demo_1')
+let h3_1 = document.getElementById('h3_1')
+let btn1 = document.getElementById('btn1')
+let h1_2 = document.getElementById('h1_2')
+let demo_2 = document.getElementById('demo_2')
+let h3_2 = document.getElementById('h3_2')
+let btn2 = document.getElementById('btn2')
+let h1_3 = document.getElementById('h1_3')
+let demo_3 = document.getElementById('demo_3')
+let h3_3 = document.getElementById('h3_3')
+let btn3 = document.getElementById('btn3')
+let h1_4 = document.getElementById('h1_4')
+let demo_4 = document.getElementById('demo_4')
+let h3_4 = document.getElementById('h3_4')
+let btn4 = document.getElementById('btn4')
+
+
+btn_1.addEventListener('click',function() {
+    h1_1.innerHTML = inp_1.value
+})
+
+btn_2.addEventListener('click', () => {
+    demo_1.innerHTML = inp_2.value
+})
+btn_3.addEventListener('click', () => {
+    h3_1.innerHTML = inp_3.value
+})
+btn_4.addEventListener('click', () => {
+    btn1.innerHTML = inp_4.value
+})
+btn_5.addEventListener('click', () => {
+    h1_2.innerHTML = inp_5.value
+})
+btn_6.addEventListener('click', () => {
+    demo_2.innerHTML = inp_6.value
+})
+btn_7.addEventListener('click', () => {
+    h3_2.innerHTML = inp_7.value
+})
+btn_8.addEventListener('click', () => {
+    btn2.innerHTML = inp_8.value
+})
+btn_9.addEventListener('click', () => {
+    h1_3.innerHTML = inp_9.value
+})
+btn_10.addEventListener('click', () => {
+    demo_3.innerHTML = inp_10.value
+})
+btn_11.addEventListener('click', () => {
+    h3_3.innerHTML = inp_11.value
+})
+btn_12.addEventListener('click', () => {
+    btn3.innerHTML = inp_12.value
+})
+btn_13.addEventListener('click', () => {
+    h1_4.innerHTML = inp_13.value
+})
+btn_14.addEventListener('click', () => {
+    demo_4.innerHTML = inp_14.value
+})
+btn_15.addEventListener('click', () => {
+    h3_4.innerHTML = inp_15.value
+})
+btn_16.addEventListener('click', () => {
+    btn4.innerHTML = inp_16.value
+})
